8ba71294bf62c0c5a23f84ad1f0ac38aac77187f
iranjith <rgranjith89@gmail.com>
2023-10-12 07:20:58 +0530

    fix: update object using emmer

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -4,18 +4,21 @@ import Alert from "./components/Alert";
 import Button from "./components/Button/Button";
 import ListGroup from "./components/ListGroup/ListGroup";
 import Like from "./components/Like";
+import produce from "immer";
 
 function App() {
-  const [tags, setTags] = useState(["happy", "cheerful"]);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "bug 1", fixed: false },
+    { id: 2, title: "bug 2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    //add
-    setTags([...tags, "exciting"]);
-    //remove
-    setTags(tags.filter((tag) => tag !== "happy"));
-    //update
-    setTags(tags.map(tag=>tag==='happy'?'happiness':'tag'));
-
+    setBugs(
+      produce((draft) => {
+        const bug = draft.find((bug) => bug.id === 1);
+        if (bug) bug.fixed = true;
+      })
+    );
   };
 
   return (
