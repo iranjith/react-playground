4ed49e38947487f1de63335b12c596f911acb0c3
iranjith <rgranjith89@gmail.com>
2023-10-12 06:55:00 +0530

    fix: updating array

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -6,24 +6,20 @@ import ListGroup from "./components/ListGroup/ListGroup";
 import Like from "./components/Like";
 
 function App() {
-  const [customer, setCustomer] = useState({
-    name: "Ranjith",
-    address: {
-      city: "San Francisco",
-      zipCode: 94111,
-    },
-  });
+  const [tags, setTags] = useState(["happy", "cheerful"]);
 
   const handleClick = () => {
-    setCustomer({
-      ...customer,
-      address: { ...customer.address, zipCode: 94112 },
-    });
+    //add
+    setTags([...tags, "exciting"]);
+    //remove
+    setTags(tags.filter((tag) => tag !== "happy"));
+    //update
+    setTags(tags.map(tag=>tag==='happy'?'happiness':'tag'));
+
   };
 
   return (
     <div>
-      {customer.address.zipCode}
       <button onClick={handleClick}>Click Me</button>
     </div>
   );
