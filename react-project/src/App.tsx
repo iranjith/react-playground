0668f5d7bb86766ee87790497efa1a5f59912213
iranjith <rgranjith89@gmail.com>
2023-10-17 22:36:10 +0530

    feat: expandable text component

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -7,15 +7,14 @@ import Like from "./components/Like";
 import produce from "immer";
 import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-
-    const [cartItems, setCartItems]= useState(['Product 1','Product 2'])
-
     return (
     <div>
-      <NavBar cartItemsCount={cartItems.length}></NavBar>
-      <Cart cartItems={cartItems} onClear={()=>setCartItems([])}></Cart>
+      <ExpandableText maxChars={50}>
+        Lorem ipsum dolo sit amet consectetur, adipisicing elit. Minima consequuntur repudiandae repellat maxime earum asperiores beatae, quibusdam molestiae incidunt veniam quisquam ut quo, nulla deleniti? Pariatur accusantium quia tempore quis qui. Adipisci quibusdam deserunt totam doloribus, cum suscipit porro itaque quod rem possimus quam non corporis tempora. Natus quasi, placeat necessitatibus, id obcaecati eius temporibus ipsum dolor dolorem itaque error similique, porro ad nulla doloremque ab dolores inventore fugiat eos illo neque delectus doloribus culpa. Fuga neque fugit similique vel voluptatibus quia inventore, optio ipsum quo temporibus, molestiae veritatis veniam possimus nihil consequatur atque eaque voluptatum necessitatibus cupiditate deserunt nemo.
+      </ExpandableText>
     </div>
   );
 }
