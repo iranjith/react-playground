1b3f7efbb6120cf4361d0da0bd55b985d2a6b457
iranjith <rgranjith89@gmail.com>
2023-10-18 06:06:25 +0530

    feat: handle form submission

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -8,13 +8,12 @@ import produce from "immer";
 import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
     return (
     <div>
-      <ExpandableText maxChars={50}>
-        Lorem ipsum dolo sit amet consectetur, adipisicing elit. Minima consequuntur repudiandae repellat maxime earum asperiores beatae, quibusdam molestiae incidunt veniam quisquam ut quo, nulla deleniti? Pariatur accusantium quia tempore quis qui. Adipisci quibusdam deserunt totam doloribus, cum suscipit porro itaque quod rem possimus quam non corporis tempora. Natus quasi, placeat necessitatibus, id obcaecati eius temporibus ipsum dolor dolorem itaque error similique, porro ad nulla doloremque ab dolores inventore fugiat eos illo neque delectus doloribus culpa. Fuga neque fugit similique vel voluptatibus quia inventore, optio ipsum quo temporibus, molestiae veritatis veniam possimus nihil consequatur atque eaque voluptatum necessitatibus cupiditate deserunt nemo.
-      </ExpandableText>
+      <Form></Form>
     </div>
   );
 }
