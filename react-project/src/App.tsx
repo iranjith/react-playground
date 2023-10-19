3fc4e36d556b48c9ce3ac3e026540823ea8018ab
iranjith <rgranjith89@gmail.com>
2023-10-19 08:40:54 +0530

    feat: build expense list

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -9,11 +9,21 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-    return (
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "baa", amount: 10, category: "Utilities" },
+    { id: 3, description: "caa", amount: 10, category: "Utilities" },
+    { id: 4, description: "daa", amount: 10, category: "Utilities" },
+  ]);
+  return (
     <div>
-      <Form></Form>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id != id))}
+      ></ExpenseList>
     </div>
   );
 }
