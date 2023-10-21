f3bac7c343358032d0a6306ff63dbd2f71135658
iranjith <rgranjith89@gmail.com>
2023-10-21 06:26:07 +0530

    feat: expense tracker feature

--- react-project/src/App.tsx
+++ react-project/src/App.tsx
@@ -12,8 +12,7 @@ import Form from "./components/Form";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-
-export const categories=['Categories','Utilities','Entertainment']
+import categories from "./expense-tracker/categories";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState("");
@@ -32,7 +31,11 @@ function App() {
   return (
     <div>
       <div className="mb-3">
-        <ExpenseForm></ExpenseForm>
+        <ExpenseForm
+          onSubmit={expense =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        ></ExpenseForm>
       </div>
 
       <div className="mb-3">
--- react-project/src/expense-tracker/components/ExpenseForm.tsx
+++ react-project/src/expense-tracker/components/ExpenseForm.tsx
@@ -2,37 +2,76 @@ import React from "react";
 import { FieldValues, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { categories } from "../../App";
+import categories from "../categories";
 
 const schema = z.object({
   description: z
     .string()
-    .min(10, { message: "Description must be at least 5 characters" }),
+    .min(3, { message: "Description must be at least 3 characters" })
+    .max(50),
   amount: z
     .number({ invalid_type_error: "Amount field is required." })
-    .min(10, { message: "Amount must be greater than 10" }),
-});
+    .min(1, { message: "Amount must be greater than 10" })
+    .max(100_000),
+  category: z.enum(categories)})
 
-const ExpenseForm = () => {
+type ExpenseFormData = z.infer<typeof schema>;
+
+interface Props {
+  onSubmit: (data: ExpenseFormData) => void;
+}
+
+const ExpenseForm = ({ onSubmit }: Props) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors, isValid },
+  } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(data=>{
+      onSubmit(data);
+      reset();
+    })}>
       <div className="mb-3">
         <label htmlFor="description" className="form-label">
           Description
         </label>
-        <input id="description" type="text" className="form-control" />
+        <input
+          {...register("description")}
+          id="description"
+          type="text"
+          className="form-control"
+        />
+        {errors.description && (
+          <p className="text-danger">{errors.description.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="amount" className="form-label">
           Amount
         </label>
-        <input id="amount" type="number" className="form-control" />
+        <input
+          {...register("amount", { valueAsNumber: true })}
+          id="amount"
+          type="number"
+          className="form-control"
+        />
+        {errors.amount && (
+          <p className="text-danger">{errors.amount.message}</p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="category" className="form-label">
           Category
         </label>
-        <select name="" id="category" className="form-select">
+        <select
+          {...register("category")}
+          name="category"
+          id="selectCategory"
+          className="form-select"
+        >
           <option value="">All Categories</option>
           {categories.map((category) => (
             <option key={category} value={category}>
@@ -40,7 +79,12 @@ const ExpenseForm = () => {
             </option>
           ))}
         </select>
-        <div className="btn btn-primary mt-3">Submit</div>
+        {errors.category && (
+          <p className="text-danger">{errors.category.message}</p>
+        )}
+        <button className="btn btn-primary mt-3" type="submit">
+          Submit
+        </button>
       </div>
     </form>
   );
